b275ba55ce7af2dcfca7ef77085dc211b5743d98
pdthorsr <sagittarius@live.no>
2017-01-28 23:41:29 +0100

    Lagt til riktig versjon av fila for akselerometer

--- experiments/accelerometer_motor_speed/main.py
+++ experiments/accelerometer_motor_speed/main.py
@@ -1,9 +1,9 @@
 tilt = 0
 while true:
     tilt = accelerometer.get_y()
-    if tilt is >1023:
+    if tilt>1023:
         pin0.write_analog(1023)
-    elif tilt is <-1023:
+    elif tilt<-1023:
         pin0.write_analog(-1023)
     else:
         pin0.write_analog(tilt)
